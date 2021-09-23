262098c1c30976d910fc4070c40b34e24e15ba5b
sqr00t <solomon.sj.yu@gmail.com>
2021-09-23 12:31:34 +0100

    completed task and passed tests

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+    #raise ValueError if list not 9 elements
+    if len(list) != 9:
+        raise ValueError('List must contain nine numbers.')
+    else:
+    #reshape list into 3x3 np array
+        arr = np.reshape(list,(3,3))
+    #initialise empty dictionary calc
+        calc = {}
+    #add keys:values to calc, with according np.operation converted to list
+        calc['mean'] = [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(arr).tolist()]
+        calc['variance'] = [np.var(arr, axis=0).tolist(), np.var(arr, axis=1).tolist(), np.var(arr).tolist()]
+        calc['standard deviation'] = [np.std(arr, axis=0).tolist(), np.std(arr, axis=1).tolist(), np.std(arr).tolist()]
+        calc['max'] = [np.max(arr, axis=0).tolist(), np.max(arr, axis=1).tolist(), np.max(arr).tolist()]
+        calc['min'] = [np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(arr).tolist()]
+        calc['sum'] = [np.sum(arr, axis=0).tolist(), np.sum(arr, axis=1).tolist(), np.sum(arr).tolist()]
+    #return dictionary calc
+    return calc
